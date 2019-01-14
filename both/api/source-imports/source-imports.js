cb9f6614a146669375330ac3a56bdf133b166a32
Sebastian Felix Zappe <hypo@me.com>
2019-01-14 18:28:25 +0100

    Fix broken moment.js import

--- both/api/source-imports/source-imports.js
+++ both/api/source-imports/source-imports.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import * as moment from 'moment';
+import moment from 'moment';
 import { _ } from 'meteor/underscore';
 import { Sources } from '/both/api/sources/sources';
 
